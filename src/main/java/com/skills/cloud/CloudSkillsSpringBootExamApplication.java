ac99d1f55b2c9cbe8a569b42ff85148e10b30072
aeonglab <aevalog@gmail.com>
2024-03-06 05:39:14 +0000

    change main

--- src/main/java/com/skills/cloud/CloudSkillsSpringBootExamApplication.java
+++ src/main/java/com/skills/cloud/CloudSkillsSpringBootExamApplication.java
@@ -17,7 +17,7 @@ public class CloudSkillsSpringBootExamApplication {
 	public HealthChecker index() {
 		
 		HealthChecker healthChecker = new HealthChecker();
-		healthChecker.setStatus("ok");
+		healthChecker.setStatus("spring");
 		
 		return healthChecker;
 	}
